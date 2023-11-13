e7ddf3e6fe0eb2b7e113d041e353a1e81a8f1957
ezekielgwamna <izzymarc@gmail.com>
2023-11-12 16:48:04 -0800

    betty errors fix

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - Writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
@@ -10,7 +10,7 @@
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+	return (write(1, &c, 1));
 }
 
 /**
@@ -20,49 +20,71 @@ int _putchar(char c)
  */
 int _printf(const char *format, ...)
 {
-    unsigned int i;
-    va_list args;
-    int count = 0;
-    char *str;
-    int j;
+	unsigned int i;
+	va_list args;
+	int count = 0;
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    count += _putchar(va_arg(args, int));
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(null)";
-                    for (j = 0; str[j]; j++)
-                    {
-                        count += _putchar(str[j]);
-                    }
-                    break;
-                case '%':
-                    count += _putchar('%');
-                    break;
-                default:
-                    count += _putchar('%');
-                    count += _putchar(format[i]);
-                    break;
-            }
-        }
-        else
-            count += _putchar(format[i]);
-    }
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					count += handle_char(args);
+					break;
+				case 's':
+					count += handle_string(args);
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(format[i]);
+			}
+		}
+		else
+			count += _putchar(format[i]);
+	}
 
-    va_end(args);
-    return count;
+	va_end(args);
+	return (count);
+}
+
+/**
+ * handle_char - Handles character format specifier
+ * @args: List of arguments
+ *
+ * Return: Number of characters printed
+ */
+static int handle_char(va_list args)
+{
+	return (_putchar(va_arg(args, int)));
+}
+
+/**
+ * handle_string - Handles string format specifier
+ * @args: List of arguments
+ *
+ * Return: Number of characters printed
+ */
+static int handle_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0, j;
+
+	if (str == NULL)
+		str = "(null)";
+	for (j = 0; str[j]; j++)
+	{
+		count += _putchar(str[j]);
+	}
+	return (count);
 }
