3db8afe2781375a1896494a5b2d4b394f17ff178
izzymarc <izzymarc@gmail.com>
2023-11-15 02:16:00 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -32,12 +32,12 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = flagInterpreter(format, &i);
-			width = breadthAnalyser(format, &i, list);
-			precision = preciseCalcModule(format, &i, list);
-			size = sizeDecipherUnit(format, &i);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
 			++i;
-			printed = printOrchestrator(format, &i, list, buffer,
+			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
