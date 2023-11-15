ca055a25a2a1555165936b5d482ad0780bb5f059
izzymarc <izzymarc@gmail.com>
2023-11-15 03:07:49 +0100

    Decipher

---
+++ flagInterpreter.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/* Modified multi-line comment start
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified multi-line comment end */
+int get_flags(const char *format, int *i)
+{
+/* Modified multi-line comment start
+/* Modified multi-line comment start
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
+ * Modified comment line
