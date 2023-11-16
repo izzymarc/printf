11927907c6a085e2f025373b9ed6d92378065107
ezekielgwamna <izzymarc@gmail.com>
2023-11-16 11:47:07 -0800

    betty printOrchestrator.c

--- printOrchestrator.c
+++ printOrchestrator.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
+ * printOrchestrator - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
  * @ind: ind.
@@ -12,7 +12,7 @@
  *
  * Return: 1 or 2;
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int printOrchestrator(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
