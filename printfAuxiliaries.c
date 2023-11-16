46279bf77972cfa0a10fc5c8c59f6ae9a6a9680a
ezekielgwamna <izzymarc@gmail.com>
2023-11-16 12:32:53 -0800

    print auxi

--- printfAuxiliaries.c
+++ printfAuxiliaries.c
@@ -15,10 +15,10 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - Append ASCII in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
+ * @ascii_code: ASCII CODE.
  *
  * Return: Always 3
  */
