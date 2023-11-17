04d63b4bbd874a764092c58f69999b7508536e8f
ezekielgwamna <izzymarc@gmail.com>
2023-11-17 12:25:13 -0800

    print file

--- _printf.c
+++ _printf.c
@@ -53,14 +53,14 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exists
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: Index at which to add the next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
+		outputConductor(buffer, *buff_ind);
 
 	*buff_ind = 0;
 }
