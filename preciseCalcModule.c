3fa684131e366d96e1d44ecb09acde838f07975a
ezekielgwamna <izzymarc@gmail.com>
2023-11-17 11:16:25 -0800

    main c deleted

--- preciseCalcModule.c
+++ preciseCalcModule.c
@@ -1,44 +1,45 @@
-#include "main.h"
-
 /*
- * preciseCalcModule - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: List of arguments.
+ * preciseCalcModule - Calculates the precision for printing.
  *
- * Return: Precision.
+ * This function calculates the precision for printing based on the provided
+ * format, index, and variable argument list.
+ *
+ * @format: Formatted string in which to print the arguments.
+ * @i: Index of the argument in the format string.
+ * @list: Variable argument list.
+ *
+ * Return: Precision value.
  */
 int preciseCalcModule(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+    int curr_i = *i + 1;
+    int precision = -1;
 
-	if (format[curr_i] != '.')
-		return (precision);
+    if (format[curr_i] != '.')
+        return (precision);
 
-	precision = 0;
+    precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+    {
+        if (is_digit(format[curr_i]))
+        {
+            precision *= 10;
+            precision += format[curr_i] - '0';
+        }
+        else if (format[curr_i] == '*')
+        {
+            precision = va_arg(list, int);
+            break;
+        }
+        else
+            break;
+    }
 
-	/* Check if precision is negative, if yes, treat it as 0 */
-	precision = (precision < 0) ? 0 : precision;
+    /* Check if precision is negative, if yes, treat it as 0 */
+    precision = (precision < 0) ? 0 : precision;
 
-	*i = curr_i - 1;
+    *i = curr_i - 1;
 
-	return (precision);
+    return (precision);
 }
